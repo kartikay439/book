6e9b4f27817191ff01463a326cf1924b6089fa35
kartikay439 <kartikaytrivedi99@gmail.com>
2024-08-04 01:48:28 +0530

    first commit

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine import URL
+import os
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
